580dd2b6463782bd4586701548c12ddc576cc803
Fabian Pappa <fabian.pappa.ext@errepar.com>
2026-03-18 23:07:40 -0300

    developing week 3 -error handling

--- database/index.js
+++ database/index.js
@@ -30,9 +30,8 @@ if (process.env.NODE_ENV === "development") {
     },
   };
 } else {
-  pool -
-    new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
   module.exports = pool;
 }
