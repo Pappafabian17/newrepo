b94012f5e5494eb2f3e8761a2a4706f251feb798
fabianpappa <fabian.pappa.ext@errepar.com>
2026-03-20 16:04:48 -0300

    w-3

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -27,4 +27,17 @@ async function getInventoryByClassificationId(classification_id) {
   }
 }
 
-module.exports = { getClassifications, getInventoryByClassificationId };
+
+async function getInventoryByInvId(inv_id){
+  const data = await pool.query(
+    `SELECT i.*, c.classification_name
+     FROM public.inventory AS i
+     JOIN public.classification AS c
+     ON i.classification_id = c.classification_id
+     WHERE i.inv_id = $1`,
+    [inv_id],
+  );
+  return data.rows[0];
+}
+
+module.exports = { getClassifications, getInventoryByClassificationId, getInventoryByInvId };
