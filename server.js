73113835af76ad294a73a5607e8ca04711a60847
Fabian Pappa <fabian.pappa.ext@errepar.com>
2026-03-23 17:58:21 -0300

    developing w-4

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities/");
+const session = require("express-session");
+const pool = require("./database/");
+const bodyParser = require("body-parser");
 
 /* ***********************
  * View Engine and Templates
@@ -22,11 +26,37 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
 /* ***********************
+ * Middleware
+ * ************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+/* ***********************
  * Routes
  *************************/
 app.use(static);
 //index route
 app.get("/", utilities.handleErrors(baseController.buildHome));
+app.use("/account", accountRoute);
 // Inventory routes
 app.use("/inv", inventoryRoute);
 
